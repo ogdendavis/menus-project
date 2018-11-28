10eb587b5002afdc7df8476feb276fb02226c85d
Lucas Ogden-Davis <lucas@ogdendavis.com>
2018-11-28 14:50:17 -0500

    Add database query to first_flask_app

--- first_flask_app.py
+++ first_flask_app.py
@@ -7,13 +7,38 @@ from flask import Flask
 # module by Python
 app = Flask(__name__)
 
-# @app.route tells Flask to execute the following function whenever it receives
-# a GET request to the indicated path on the server. This will show the "Hello,
-# World! text when a user navigates to the home directory or /hello"
+# Import SQLAlchemy tools to connect to and communicate with database
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# Import classes from database creation file
+from database_setup import Base, Restaurant, MenuItem
+
+# Connect to database and connect tables to Base classes
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+# Create a sessionmaker -- each function will create its own session
+DBSession = sessionmaker(bind = engine)
+
+# @app.route tells Flask to execute the following function when it receives
+# a GET request to the indicated path. You can chain them as shown
 @app.route('/')
 @app.route('/hello')
 def HelloWorld():
-    return 'Hello, World!'
+    # This will get the first restaurant, and display all its menu items
+    session = DBSession()
+    first_restaurant = session.query(Restaurant).order_by('name').first()
+    menu_items = session.query(MenuItem).filter_by(restaurant_id = first_restaurant.id).all()
+    # Now that we have the menu items, print everything out!
+    output = ''
+    output += '<h1>{}</h1>'.format(first_restaurant.name)
+    output += '<ul>'
+    for item in menu_items:
+        output += '<li>{}</li>'.format(item.name)
+    output += '</ul>'
+    # Flask handles all the headers, wfile, etc. - so just return the content!
+    return output
 
 # If module is executed from the Python shell, do this stuff. If it's imported
 # into some other code, don't do this stuff!
