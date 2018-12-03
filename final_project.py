f449d909f8c8f81ede4b89632434d90190dd6880
Lucas Ogden-Davis <lucas@ogdendavis.com>
2018-12-03 10:08:12 -0500

    Add working queries for GET requests

--- final_project.py
+++ final_project.py
@@ -2,22 +2,25 @@
 
 # Dependencies: Flask, SQLAlchemy, SQLite3
 # Uses database created in database_setup.py, and expanded by lotsofmenus.py
-from flask import Flask, render_template, request
 
 # Set up Flask instance
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
-### Temporary dummy data to use in render_template so that I can set up the
-### templates before setting up the database functionality:
+# Import SQLAlchemy tools
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-#Fake Restaurants
-f_restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
-f_restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
+# Import classes from database creation file
+from database_setup import Base, Restaurant, MenuItem
 
+# Connect to database and connect tables to Base classes
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
 
-#Fake Menu Items
-f_item = {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree', 'id':'1'}
-f_items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
+# Create a database session to use in the app
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
 
 
 # Routes!
@@ -25,7 +28,8 @@ f_items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'pri
 @app.route('/')
 @app.route('/restaurants/')
 def showRestaurants():
-    return render_template('restaurants.html', restaurants = f_restaurants)
+    restaurants = session.query(Restaurant).all()
+    return render_template('restaurants.html', restaurants = restaurants)
 
 
 # Add a new restaurant
@@ -41,54 +45,64 @@ def addRestaurant():
 # Edit a restaurant
 @app.route('/restaurants/<int:restaurant_id>/edit/', methods = ['GET', 'POST'])
 def editRestaurant(restaurant_id):
+    # For GET or POST, we'll need the restaurant
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     if request.method == 'POST':
         return 'Successful post to /restaurants/x/edit/'
     if request.method == 'GET':
-        return render_template('editrestaurant.html', restaurant = f_restaurant)
+        return render_template('editrestaurant.html', restaurant = restaurant)
 
 
 # Delete a restaurant
 @app.route('/restaurants/<int:restaurant_id>/delete/', methods = ['GET', 'POST'])
 def deleteRestaurant(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     if request.method == 'POST':
         return 'Successful post to /restaurants/x/delete/'
     if request.method == 'GET':
-        return render_template('deleterestaurant.html', restaurant = f_restaurant)
+        return render_template('deleterestaurant.html', restaurant = restaurant)
 
 
 # Show a restaurant's full menu
 @app.route('/restaurants/<int:restaurant_id>/')
 @app.route('/restaurants/<int:restaurant_id>/menu/')
 def showMenu(restaurant_id):
-    return render_template('menu.html', restaurant = f_restaurant, items = f_items)
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id).all()
+    return render_template('menu.html', restaurant = restaurant, items = items)
 
 
 # Add a menu item to a restaurant's menu
 @app.route('/restaurants/<int:restaurant_id>/menu/new/', methods = ['GET', 'POST'])
 @app.route('/restaurants/<int:restaurant_id>/menu/add/', methods = ['GET', 'POST'])
 def addItem(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     if request.method == 'POST':
         return 'Successful POST to /restaurants/x/menu/add/'
     if request.method == 'GET':
-        return render_template('additem.html', restaurant = f_restaurant)
+        return render_template('additem.html', restaurant = restaurant)
 
 
 # Edit a menu item
 @app.route('/restaurants/<int:restaurant_id>/menu/<int:item_id>/edit/', methods = ['GET', 'POST'])
 def editItem(restaurant_id, item_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    item = session.query(MenuItem).filter_by(id = item_id).one()
     if request.method == 'POST':
         return 'Successful POST to /restaurants/x/menu/y/edit/'
     if request.method == 'GET':
-        return render_template('edititem.html', restaurant = f_restaurant, item = f_item)
+        return render_template('edititem.html', restaurant = restaurant, item = item)
 
 
 #Delete a menu item
 @app.route('/restaurants/<int:restaurant_id>/menu/<int:item_id>/delete/', methods = ['GET', 'POST'])
 def deleteItem(restaurant_id, item_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    item = session.query(MenuItem).filter_by(id = item_id).one()
     if request.method == 'POST':
         return 'Successful POST to /restaurants/x/menu/y/delete'
     if request.method == 'GET':
-        return render_template('deleteitem.html', restaurant = f_restaurant, item = f_item)
+        return render_template('deleteitem.html', restaurant = restaurant, item = item)
 
 
 # When running as the web server, run in debug mode on localhost, port 5000
