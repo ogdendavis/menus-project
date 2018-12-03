90efcce4f93cbcb994e716776b162daf013e2a87
Lucas Ogden-Davis <lucas@ogdendavis.com>
2018-12-03 10:42:23 -0500

    Add working queries for POST requests

--- final_project.py
+++ final_project.py
@@ -36,8 +36,14 @@ def showRestaurants():
 @app.route('/restaurants/new/', methods = ['GET', 'POST'])
 @app.route('/restaurants/add/', methods = ['GET', 'POST'])
 def addRestaurant():
+    # In Flask, can access data sent in HTTP request with request object. Useful
+    # for screening by method (GET vs POST), and for teasing out info submitted
+    # in a form via request.form -- both of which are done below
     if request.method == 'POST':
-        return 'successful post to /restaurants/add/'
+        new_restaurant = Restaurant(name = request.form['name'])
+        session.add(new_restaurant)
+        session.commit()
+        return redirect(url_for('showRestaurants'))
     if request.method == 'GET':
         return render_template('addrestaurant.html')
 
@@ -48,7 +54,10 @@ def editRestaurant(restaurant_id):
     # For GET or POST, we'll need the restaurant
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     if request.method == 'POST':
-        return 'Successful post to /restaurants/x/edit/'
+        restaurant.name = request.form['name']
+        session.add(restaurant)
+        session.commit()
+        return redirect(url_for('showRestaurants'))
     if request.method == 'GET':
         return render_template('editrestaurant.html', restaurant = restaurant)
 
@@ -58,7 +67,9 @@ def editRestaurant(restaurant_id):
 def deleteRestaurant(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     if request.method == 'POST':
-        return 'Successful post to /restaurants/x/delete/'
+        session.delete(restaurant)
+        session.commit()
+        return redirect(url_for('showRestaurants'))
     if request.method == 'GET':
         return render_template('deleterestaurant.html', restaurant = restaurant)
 
@@ -78,7 +89,12 @@ def showMenu(restaurant_id):
 def addItem(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     if request.method == 'POST':
-        return 'Successful POST to /restaurants/x/menu/add/'
+        # Note that here, as in addRestaurant, we don't need to provide a
+        # unique ID for the new thing -- the database does that for us
+        new_item = MenuItem(name = request.form['name'], course = request.form['course'], description = request.form['description'], price = request.form['price'], restaurant_id = restaurant_id)
+        session.add(new_item)
+        session.commit()
+        return redirect(url_for('showMenu', restaurant_id = restaurant_id))
     if request.method == 'GET':
         return render_template('additem.html', restaurant = restaurant)
 
@@ -89,7 +105,18 @@ def editItem(restaurant_id, item_id):
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     item = session.query(MenuItem).filter_by(id = item_id).one()
     if request.method == 'POST':
-        return 'Successful POST to /restaurants/x/menu/y/edit/'
+        # When I try to use a 'for name, value in item' loop, I get an error
+        # that MenuItem objects aren't iterable. So I'm manually going over
+        # each value in the object and setting it to the value that was given
+        # in the form -- since I'm doing it manually, checking first and then
+        # setting seems silly
+        item.name = request.form['name']
+        item.course = request.form['course']
+        item.description = request.form['description']
+        item.price = request.form['price']
+        session.add(item)
+        session.commit()
+        return redirect(url_for('showMenu', restaurant_id = restaurant_id))
     if request.method == 'GET':
         return render_template('edititem.html', restaurant = restaurant, item = item)
 
@@ -100,7 +127,9 @@ def deleteItem(restaurant_id, item_id):
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     item = session.query(MenuItem).filter_by(id = item_id).one()
     if request.method == 'POST':
-        return 'Successful POST to /restaurants/x/menu/y/delete'
+        session.delete(item)
+        session.commit()
+        return redirect(url_for('showMenu', restaurant_id = restaurant_id))
     if request.method == 'GET':
         return render_template('deleteitem.html', restaurant = restaurant, item = item)
 
